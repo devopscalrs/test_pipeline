f7eb54bd203aa521cda72eb544914a3dac03d58f
devopscalrs <devopscalrs@gmail.com>
2021-06-22 23:40:49 -0400

    nueva carga

---
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -0,0 +1,21 @@
+
+package com.paymentchain.billing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class BasicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
+
+
+//Se ha guardado
